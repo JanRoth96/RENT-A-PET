70075a42033bc4a01aa0679db58b664b81a61fe0
Daniel Domingo <domingo.daniel21@gmail.com>
2022-08-16 12:28:18 +0900

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  resources :pets, except: [:edit, :update] do
+    resources :bookings, only: [:new, :create, :index]
+  end
+
+  resources :bookings, only: [:index, :destroy, :edit, :update]
+
+  resources :shelters do
+    resources :pets, only: [:index]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
 end
