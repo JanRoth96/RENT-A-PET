053f9c14e050351d2c83d4ff5614fae8baf7d337
Jan Roth <jan-gabriel.roth@gmx.de>
2022-08-16 12:09:51 +0900

    added validations for booking, shelter and pet. also added new column description to pet

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,6 +1,8 @@
 class Shelter < ApplicationRecord
   belongs_to :user
-
   has_many :pets
   has_many :bookings, through: :pets
+
+  validates :name, presence: true
+  valudates :location, presence: true
 end
