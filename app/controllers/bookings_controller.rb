8c967b2c88d7aeea797392d01ac9736fb75ab857
Jan Roth <jan-gabriel.roth@gmx.de>
2022-08-16 16:30:48 +0900

    added show controller action for booking and show page in views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,12 +3,22 @@ class BookingsController < ApplicationController
     @bookings = Booking.all
   end
 
+  def show
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
+
   # def new
   #   @booking = Booking.new
   #   authorize @booking
   # end
 
   # def create
-
+  #   @pet = Pet.new(pet_params)
+  #   if @pet.save
+  #     redirect_to pet_path(@pet)
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
   # end
 end
