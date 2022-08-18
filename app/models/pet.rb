08152f0e85a1e48fa839dadfe026a153211aff08
Daniel Domingo <domingo.daniel21@gmail.com>
2022-08-18 11:56:12 +0900

    finished search

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -8,4 +8,12 @@ class Pet < ApplicationRecord
   validates :age, presence: true
   validates :price, presence: true
   validates :description, presence: true, length: { minimum: 6 }
+
+  include PgSearch::Model
+  pg_search_scope :search_by_species,
+  against: [ :species ],
+  using: {
+    tsearch: { prefix: true } # <-- now `superman batm` will return something!
+  }
+
 end
